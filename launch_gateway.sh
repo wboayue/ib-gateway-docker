666e8a38da36ba9b37e7da6d3ce48ea97fa06ed5
Wil Boayue <wilfred.boayue@airbus-sv.com>
2023-02-12 12:06:10 -0800

    chore: cleanup

--- launch_gateway.sh
+++ launch_gateway.sh
@@ -11,21 +11,22 @@ if [[ -z "${IB_PASSWORD}" ]]; then
     exit 1
 fi
 
+# Starts a virtual desktop.
+
+Xvfb ${DISPLAY} -ac -screen 0 1024x768x16 &
+
+# Starts VNC server so virtual desktop can be viwed.
+
+x11vnc -ncache_cr -display ${DISPLAY} -forever -shared -logappend /var/log/x11vnc.log -bg -noipv6
+
+# Configures and starts IB Gateway using IBC.
+
 TRADING_MODE="${TRADING_MODE:-live}"
 IBC_INI=$IBC_PATH/config.ini
-
-# Configure startup script
 
 mkdir -p ~/ibc
 sed -e "s/IbLoginId=edemo/IbLoginId=${IB_LOGIN}/;s/IbPassword=demouser/IbPassword=${IB_PASSWORD}/;s/TradingMode=live/TradingMode=${TRADING_MODE}/" > ~/ibc/config.ini
 
-Xvfb ${DISPLAY} -ac -screen 0 1024x768x16 &
-
-# Start VNC server. This allows desktop to be viewed.
-#  -passwd "$VNC_SERVER_PASSWORD"
-x11vnc -ncache_cr -display ${DISPLAY} -forever -shared -logappend /var/log/x11vnc.log -bg -noipv6
-
-# Configure and start IB Gateway
-
 sed --in-place=.bak -e "s/TWS_MAJOR_VRSN=1019/TWS_MAJOR_VRSN=${TWS_MAJOR_VRSN}/" /opt/ibc/gatewaystart.sh
+
 /opt/ibc/gatewaystart.sh -inline
