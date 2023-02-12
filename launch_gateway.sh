42191a1bd1728bdb8c4bc0b57935461aaad8d532
Wil Boayue <wilfred.boayue@airbus-sv.com>
2023-02-12 12:05:24 -0800

    chore: cleanup

--- launch_gateway.sh
+++ launch_gateway.sh
@@ -12,34 +12,20 @@ if [[ -z "${IB_PASSWORD}" ]]; then
 fi
 
 TRADING_MODE="${TRADING_MODE:-live}"
-
 IBC_INI=$IBC_PATH/config.ini
 
 # Configure startup script
+
 mkdir -p ~/ibc
 sed -e "s/IbLoginId=edemo/IbLoginId=${IB_LOGIN}/;s/IbPassword=demouser/IbPassword=${IB_PASSWORD}/;s/TradingMode=live/TradingMode=${TRADING_MODE}/" > ~/ibc/config.ini
 
 Xvfb ${DISPLAY} -ac -screen 0 1024x768x16 &
 
+# Start VNC server. This allows desktop to be viewed.
 #  -passwd "$VNC_SERVER_PASSWORD"
 x11vnc -ncache_cr -display ${DISPLAY} -forever -shared -logappend /var/log/x11vnc.log -bg -noipv6
 
-# sleep 30
-
-# if [ "$TRADING_MODE" = "paper" ]; then
-#   printf "Forking :::4000 onto 0.0.0.0:4002\n"
-#   socat TCP-LISTEN:4002,fork TCP:127.0.0.1:4000
-# else
-#   printf "Forking :::4000 onto 0.0.0.0:4001\n"
-#   socat TCP-LISTEN:4001,fork TCP:127.0.0.1:4000
-# fi
+# Configure and start IB Gateway
 
 sed --in-place=.bak -e "s/TWS_MAJOR_VRSN=1019/TWS_MAJOR_VRSN=${TWS_MAJOR_VRSN}/" /opt/ibc/gatewaystart.sh
-cat /opt/ibc/gatewaystart.sh
-
-mkdir -p /root/ibc/logs/
-touch /root/ibc/logs/ibc-3.16.0_GATEWAY-1020_Saturday.txt
-tail -f /root/ibc/logs/ibc-3.16.0_GATEWAY-1020_Saturday.txt &
-
 /opt/ibc/gatewaystart.sh -inline
-
